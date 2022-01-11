9ef51c02c257509ae2227d4f00fcff34a5991071
Pratik Magar <67797986+magarpratik@users.noreply.github.com>
2022-01-11 13:05:33 +0000

    amended tabs to display colour when selected

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,16 +1,30 @@
 import React from "react";
-import { Box, LinkTab, Tab, Tabs, TabPanel } from "@mui/material";
+import { Box, Tab, Tabs } from "@mui/material";
 
 const NavBar = () => {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
   return (
-    <Tabs centered variant="scrollable" scrollButtons="auto">
-      <Tab label="All Articles" />
-      <Tab label="Football" />
-      <Tab label="Politics" />
-      <Tab label="All Articles" />
-      <Tab label="Football" />
-      <Tab label="Politics" />
-    </Tabs>
+    <Box sx={{ maxWidth: 480, bgcolor: "background.paper" }}>
+      <Tabs
+        value={value}
+        onChange={handleChange}
+        variant="scrollable"
+        scrollButtons="auto"
+      >
+        <Tab label="Item One" />
+        <Tab label="Item Two" />
+        <Tab label="Item Three" />
+        <Tab label="Item Four" />
+        <Tab label="Item Five" />
+        <Tab label="Item Six" />
+        <Tab label="Item Seven" />
+      </Tabs>
+    </Box>
   );
 };
 
