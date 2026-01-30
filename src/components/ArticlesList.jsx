efabfcaab9d4482fa3f7a0f054b6f86f005e5757
Pratik Magar <magarpratik@live.co.uk>
2026-01-30 21:59:23 +0000

    fix article images

--- src/components/ArticlesList.jsx
+++ src/components/ArticlesList.jsx
@@ -122,7 +122,7 @@ const ArticlesList = ({ topic }) => {
                     }}
                   >
                     <CardMedia
-                      image={`https://picsum.photos/${crypto.randomUUID()}/800/600`}
+                      image={`https://picsum.photos/${article.article_id}/800/600`}
                       title={article.title}
                       sx={{
                         paddingTop: "56.25%",
